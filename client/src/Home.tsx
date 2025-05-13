7d2aaa7468bd7834a83c161d579d9e1fb377276f
りおちん <118029.ichikama@gmail.com>
2025-05-13 13:44:38 +0900

    fix: ランキング表示のレイアウト・サイズ調整の反映

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -14,45 +14,40 @@ const dummyParticipants = [
 
 const Home = () => {
     return (
-        <div className="home__container">
-            <SidebarDrawer />
-            <TopTitle title="ハッカソンもくもくかい①" />
-            <div style={{flex:1, width:'100%'}}>
-                <div className="home__header">
-                    <span className="home__title">ハッカソンもくもくかい①</span>
-                    <span className="home__timer">1:04:23</span>
-                </div>
-                <div className="home__contribution">Contribution <span>23</span></div>
-                <div className="home__chat">
-                    <div className="home__chat-item home__chat-item--left">
-                        <img src={logo} alt="avatar" className="home__avatar" />
-                        <div className="home__bubble home__bubble--blue">
-                            Issue tateta...
-                        </div>
-                        <span className="home__time">17:21</span>
+        <div style={{position: 'relative', width: '100vw', height: '100vh', overflow: 'hidden'}}>
+            <ParticipantRanking participants={dummyParticipants} />
+            <div className="home__container">
+                <SidebarDrawer />
+                <TopTitle title="ハッカソンもくもくかい①" />
+                <div style={{flex:1, width:'100%'}}>
+                    <div className="home__header">
+                        <span className="home__title">ハッカソンもくもくかい①</span>
+                        <span className="home__timer">1:04:23</span>
                     </div>
-                    <div className="home__chat-item home__chat-item--left">
-                        <img src={logo} alt="avatar" className="home__avatar" />
-                        <div className="home__bubble home__bubble--blue">
-                            Issue tateta...
+                    <div className="home__contribution">Contribution <span>23</span></div>
+                    <div className="home__chat">
+                        <div className="home__chat-item home__chat-item--left">
+                            <img src={logo} alt="avatar" className="home__avatar" />
+                            <div className="home__bubble home__bubble--blue">
+                                Issue tateta...
+                            </div>
+                            <span className="home__time">17:21</span>
                         </div>
-                        <span className="home__time">17:21</span>
-                    </div>
-                    <div className="home__chat-item home__chat-item--right">
-                        <img src={logo} alt="avatar" className="home__avatar" />
-                        <div className="home__bubble home__bubble--green">
-                            pushed 2 commits!<br />Add: emoi modal...
+                        <div className="home__chat-item home__chat-item--left">
+                            <img src={logo} alt="avatar" className="home__avatar" />
+                            <div className="home__bubble home__bubble--blue">
+                                Issue tateta...
+                            </div>
+                            <span className="home__time">17:21</span>
                         </div>
-                        <span className="home__time">17:43</span>
+                        <div className="home__chat-item home__chat-item--right">
+                            <img src={logo} alt="avatar" className="home__avatar" />
+                            <div className="home__bubble home__bubble--green">
+                                pushed 2 commits!<br />Add: emoi modal...
+                            </div>
+                            <span className="home__time">17:43</span>
+                        </div>
                     </div>
-                </div>
-                <ParticipantRanking participants={dummyParticipants} />
-                <div className="home__footer">
-                    <img src={logo} alt="icon1" className="home__footer-icon" />
-                    <img src={logo} alt="icon2" className="home__footer-icon" />
-                    <img src={logo} alt="icon3" className="home__footer-icon" />
-                    <button className="home__footer-btn">休</button>
-                    <img src={logo} alt="icon4" className="home__footer-icon" />
                 </div>
             </div>
         </div>
--- client/src/components/ParticipantRanking.tsx
+++ client/src/components/ParticipantRanking.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../Home.css';
+import '../Home';
 import { FaCrown } from 'react-icons/fa';
 
 type Participant = {
@@ -13,24 +13,30 @@ type ParticipantRankingProps = {
 };
 
 const crownColors = ['#FFD700', '#C0C0C0', '#CD7F32']; // 金・銀・銅
+const iconSizes = [50, 40, 35, 30]; // 1位,2位,3位,4位以降
+const crownSizes = [14, 12, 10, 8];
 
 const ParticipantRanking: React.FC<ParticipantRankingProps> = ({ participants }) => {
     // commit数で降順ソート
     const sorted = [...participants].sort((a, b) => b.commit - a.commit).slice(0, 10);
     return (
-        <div className="home__footer">
-            {sorted.map((p, i) => (
-                <div key={p.username} className="home__footer-icon-wrapper">
-                    <div style={{position:'relative', display:'inline-block'}}>
-                        <img src={p.iconUrl} alt={p.username} className="home__footer-icon" />
-                        {i < 3 && (
-                            <FaCrown className="home__footer-crown" style={{ color: crownColors[i] }} />
-                        )}
+        <div className="home__footer home__footer--left">
+            {sorted.map((p, i) => {
+                const iconSize = (i < 3 ? iconSizes[i] : iconSizes[3]) + 'px';
+                const crownSize = (i < 3 ? crownSizes[i] : crownSizes[3]) + 'px';
+                return (
+                    <div key={p.username} className="home__footer-icon-wrapper">
+                        <div style={{position:'relative', display:'inline-block'}}>
+                            <img src={p.iconUrl} alt={p.username} className="home__footer-icon" style={{width: iconSize, height: iconSize}} />
+                            {i < 3 && (
+                                <FaCrown className="home__footer-crown" style={{ color: crownColors[i], fontSize: crownSize }} />
+                            )}
+                        </div>
+                        <div className="home__footer-username">{p.username}</div>
+                        <div className="home__footer-commit">{p.commit} commits</div>
                     </div>
-                    <div className="home__footer-username">{p.username}</div>
-                    <div className="home__footer-commit">{p.commit} commits</div>
-                </div>
-            ))}
+                );
+            })}
         </div>
     );
 };
