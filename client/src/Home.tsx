683c92a5c99b272f071e6a3d1cbf0ddc3fc62d26
りおちん <118029.ichikama@gmail.com>
2025-05-13 10:18:30 +0900

    feat: サイドバー引き出し要素をSidebarDrawerコンポーネントとして分離

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,43 +1,47 @@
 import './Home.css';
 import logo from './assets/logo.png';
+import SidebarDrawer from './components/SidebarDrawer';
 
 const Home = () => {
     return (
         <div className="home__container">
-            <div className="home__header">
-                <span className="home__title">ハッカソンもくもくかい①</span>
-                <span className="home__timer">1:04:23</span>
-            </div>
-            <div className="home__contribution">Contribution <span>23</span></div>
-            <div className="home__chat">
-                <div className="home__chat-item home__chat-item--left">
-                    <img src={logo} alt="avatar" className="home__avatar" />
-                    <div className="home__bubble home__bubble--blue">
-                        Issue tateta...
-                    </div>
-                    <span className="home__time">17:21</span>
+            <SidebarDrawer />
+            <div style={{flex:1, width:'100%'}}>
+                <div className="home__header">
+                    <span className="home__title">ハッカソンもくもくかい①</span>
+                    <span className="home__timer">1:04:23</span>
                 </div>
-                <div className="home__chat-item home__chat-item--left">
-                    <img src={logo} alt="avatar" className="home__avatar" />
-                    <div className="home__bubble home__bubble--blue">
-                        Issue tateta...
+                <div className="home__contribution">Contribution <span>23</span></div>
+                <div className="home__chat">
+                    <div className="home__chat-item home__chat-item--left">
+                        <img src={logo} alt="avatar" className="home__avatar" />
+                        <div className="home__bubble home__bubble--blue">
+                            Issue tateta...
+                        </div>
+                        <span className="home__time">17:21</span>
                     </div>
-                    <span className="home__time">17:21</span>
-                </div>
-                <div className="home__chat-item home__chat-item--right">
-                    <img src={logo} alt="avatar" className="home__avatar" />
-                    <div className="home__bubble home__bubble--green">
-                        pushed 2 commits!<br />Add: emoi modal...
+                    <div className="home__chat-item home__chat-item--left">
+                        <img src={logo} alt="avatar" className="home__avatar" />
+                        <div className="home__bubble home__bubble--blue">
+                            Issue tateta...
+                        </div>
+                        <span className="home__time">17:21</span>
                     </div>
-                    <span className="home__time">17:43</span>
+                    <div className="home__chat-item home__chat-item--right">
+                        <img src={logo} alt="avatar" className="home__avatar" />
+                        <div className="home__bubble home__bubble--green">
+                            pushed 2 commits!<br />Add: emoi modal...
+                        </div>
+                        <span className="home__time">17:43</span>
+                    </div>
                 </div>
-            </div>
-            <div className="home__footer">
-                <img src={logo} alt="icon1" className="home__footer-icon" />
-                <img src={logo} alt="icon2" className="home__footer-icon" />
-                <img src={logo} alt="icon3" className="home__footer-icon" />
-                <button className="home__footer-btn">休</button>
-                <img src={logo} alt="icon4" className="home__footer-icon" />
+                <div className="home__footer">
+                    <img src={logo} alt="icon1" className="home__footer-icon" />
+                    <img src={logo} alt="icon2" className="home__footer-icon" />
+                    <img src={logo} alt="icon3" className="home__footer-icon" />
+                    <button className="home__footer-btn">休</button>
+                    <img src={logo} alt="icon4" className="home__footer-icon" />
+                </div>
             </div>
         </div>
     );
---
+++ client/src/components/SidebarDrawer.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { FaMusic, FaHourglass, FaChevronRight, FaChevronLeft } from 'react-icons/fa';
+import '../Home.css';
+
+const SidebarDrawer: React.FC = () => {
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    return (
+        <div className={`sidebar${drawerOpen ? ' sidebar--open' : ''}`}> 
+            <button className="sidebar__toggle" onClick={() => setDrawerOpen(!drawerOpen)}>
+                {drawerOpen ? <FaChevronLeft /> : <FaChevronRight />}
+            </button>
+            {drawerOpen && (
+                <div className="sidebar__icons">
+                    <FaMusic className="sidebar__icon" />
+                    <FaHourglass className="sidebar__icon" />
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SidebarDrawer; 
