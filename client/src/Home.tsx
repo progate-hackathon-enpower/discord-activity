9eefc6f7fd38891a2e185ae0eac79a3edf1ac654
りおちん <118029.ichikama@gmail.com>
2025-05-13 09:36:57 +0900

    fix: Home画面をアクティビティ横幅いっぱいに広がるよう修正

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,12 +1,43 @@
 import './Home.css';
+import logo from './assets/logo.png';
 
 const Home = () => {
     return (
-        <div className="home">
-            <div>
-                <h1>Welcome to the Discord Activity</h1>
-                <p>CCCCCCCCClick the button below to start your adventure!</p>
-                <button className="home__button">Start</button>
+        <div className="home__container">
+            <div className="home__header">
+                <span className="home__title">ハッカソンもくもくかい①</span>
+                <span className="home__timer">1:04:23</span>
+            </div>
+            <div className="home__contribution">Contribution <span>23</span></div>
+            <div className="home__chat">
+                <div className="home__chat-item home__chat-item--left">
+                    <img src={logo} alt="avatar" className="home__avatar" />
+                    <div className="home__bubble home__bubble--blue">
+                        Issue tateta...
+                    </div>
+                    <span className="home__time">17:21</span>
+                </div>
+                <div className="home__chat-item home__chat-item--left">
+                    <img src={logo} alt="avatar" className="home__avatar" />
+                    <div className="home__bubble home__bubble--blue">
+                        Issue tateta...
+                    </div>
+                    <span className="home__time">17:21</span>
+                </div>
+                <div className="home__chat-item home__chat-item--right">
+                    <img src={logo} alt="avatar" className="home__avatar" />
+                    <div className="home__bubble home__bubble--green">
+                        pushed 2 commits!<br />Add: emoi modal...
+                    </div>
+                    <span className="home__time">17:43</span>
+                </div>
+            </div>
+            <div className="home__footer">
+                <img src={logo} alt="icon1" className="home__footer-icon" />
+                <img src={logo} alt="icon2" className="home__footer-icon" />
+                <img src={logo} alt="icon3" className="home__footer-icon" />
+                <button className="home__footer-btn">休</button>
+                <img src={logo} alt="icon4" className="home__footer-icon" />
             </div>
         </div>
     );
