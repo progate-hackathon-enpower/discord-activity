02f52e41e15e2365ccd2b7645372db673e67ca9c
りおちん <118029.ichikama@gmail.com>
2025-05-17 08:52:01 +0900

    fix: toptitle.tsxが復活

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['dependence-mud-bunch-brief.trycloudflare.com'],
+    allowedHosts: ['nr-acceptable-month-innovations.trycloudflare.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
