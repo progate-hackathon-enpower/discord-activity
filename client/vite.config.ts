4f2a74e69e195238af0ec3a2a40f99d854c63e60
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-05-18 08:49:27 +0900

    Add: icon

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+<<<<<<< Updated upstream
     allowedHosts: ['texts-statistics-parts-phases.trycloudflare.com'],
+=======
+    allowedHosts: ['purchased-shapes-reduce-removable.trycloudflare.com'],
+>>>>>>> Stashed changes
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
