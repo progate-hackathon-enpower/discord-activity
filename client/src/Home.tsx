4f5596c255ad524a6e43f8721cd4cf282c217904
mono0218 <mono@monokamo.com>
2025-05-17 15:07:36 +0900

    Add: Vercel.json

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef, useState } from 'react';
 import './Home.css';
 import SidebarDrawer from './components/SidebarDrawer';
-import TopTitle from './components/topTitle';
+import TopTitle from './components/Title';
 import ParticipantRanking, { type ActivityUser }from './components/ParticipantRanking';
 
 import ActivityTimeline from './components/ActivityTimeline';
