36b78e0cc1fa74594383fcc47d1bc2f1c0162972
りおちん <118029.ichikama@gmail.com>
2025-05-18 09:35:18 +0900

    Fix: z-index修正してtaptostartを改善

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['texts-statistics-parts-phases.trycloudflare.com'],
+    allowedHosts: ['bosnia-encourage-solely-wisconsin.trycloudflare.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
