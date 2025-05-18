9249512eee46fa5a03f83faf8ffeb639e680b502
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-05-18 09:51:12 +0900

    Feat: Insert activity info

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-<<<<<<< Updated upstream
-    allowedHosts: ['texts-statistics-parts-phases.trycloudflare.com'],
-=======
-    allowedHosts: ['purchased-shapes-reduce-removable.trycloudflare.com'],
->>>>>>> Stashed changes
+    allowedHosts: ['conventional-jet-instrumental-powder.trycloudflare.com'],
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
